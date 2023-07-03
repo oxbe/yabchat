e0ad269c189064ff6b35ae870337df709f41bf8d
Alexander Rüedlinger <alexander.rueedlinger@zuehlke.com>
2023-07-04 00:42:13 +0200

    update CreateChatroomDTO

--- src/main/kotlin/ch/oxb/yabchat/adapters/rest/dtos/CreateChatroomDTO.kt
+++ src/main/kotlin/ch/oxb/yabchat/adapters/rest/dtos/CreateChatroomDTO.kt
@@ -2,5 +2,4 @@ package ch.oxb.yabchat.adapters.rest.dtos
 
 import ch.oxb.yabchat.business.user.User
 
-class CreateChatroomDTO(val name: String, val description: String, val users: List<User>) {
-}
+class CreateChatroomDTO(val name: String, val description: String)
