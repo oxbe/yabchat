407ba20c232e146595a2bf367beb48c23724ef37
Alexander Rüedlinger <alexander.rueedlinger@zuehlke.com>
2023-07-04 16:12:57 +0200

    add graphql query endpoints

--- src/main/kotlin/ch/oxb/yabchat/adapters/graphql/chatroom/ChatroomResource.kt
+++ src/main/kotlin/ch/oxb/yabchat/adapters/graphql/chatroom/ChatroomResource.kt
@@ -2,10 +2,13 @@ package ch.oxb.yabchat.adapters.graphql.chatroom
 
 import ch.oxb.yabchat.business.chatroom.Chatroom
 import ch.oxb.yabchat.business.chatroom.ChatroomService
+import ch.oxb.yabchat.business.user.User
+import ch.oxb.yabchat.business.user.UserService
 import jakarta.inject.Inject
 import org.eclipse.microprofile.graphql.Description
 import org.eclipse.microprofile.graphql.GraphQLApi
 import org.eclipse.microprofile.graphql.Query
+import org.eclipse.microprofile.graphql.Source
 
 @GraphQLApi
 class ChatroomResource {
@@ -13,9 +16,17 @@ class ChatroomResource {
     @Inject
     lateinit var chatroomService: ChatroomService
 
+    @Inject
+    lateinit var userService: UserService
+
     @Query("allChatrooms")
     @Description("Get all chatrooms")
     fun getAllChatrooms(): List<Chatroom> {
         return chatroomService.getChatrooms()
+    }
+
+    fun users(@Source chatroom: Chatroom): List<User> {
+        return userService.getUsers()
+            .filter { user: User ->  chatroom.userIds.contains(user.id) }
     }
 }
